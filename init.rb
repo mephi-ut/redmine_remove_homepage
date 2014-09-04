e3939fbcace4ba8361477a02ac6a1b3d2aafeb78
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-09-04 13:42:29 +0400

    Fixed GitHub URL

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'redmine'
 Redmine::Plugin.register :redmine_remove_home_page do
   name 'Redmine remove Homepage'
   description 'A plugin to remove the homepage link from the top-menu.'
-  url 'https://github.com/xaionaro/redmine_remove_home_page'
+  url 'https://github.com/mephi-ut/redmine_remove_home_page'
   author 'Dmitry Yu Okunev'
   author_url 'https://github.com/xaionaro'
   version '0.0.1'
