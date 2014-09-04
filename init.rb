92454cf486f9a6a655a46048b399d9f392ae2892
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-09-04 13:41:53 +0400

    Initial commit

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_remove_home_page do
+  name 'Redmine remove Homepage'
+  description 'A plugin to remove the homepage link from the top-menu.'
+  url 'https://github.com/xaionaro/redmine_remove_home_page'
+  author 'Dmitry Yu Okunev'
+  author_url 'https://github.com/xaionaro'
+  version '0.0.1'
+  delete_menu_item :top_menu, :home
+end
+
